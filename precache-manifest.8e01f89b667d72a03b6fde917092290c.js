6fa125118b58495fc62e0e87cf7577588b831a50
unknown <samuelairadion01@gmail.com>
2020-04-21 11:19:39 +0100

    Updates

---
+++ precache-manifest.8e01f89b667d72a03b6fde917092290c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02d0b4f3e6e9d8c777373cf90bd353a",
+    "url": "/Success/index.html"
+  },
+  {
+    "revision": "4c6b3de7dde2b6a56b0d",
+    "url": "/Success/static/css/main.4b7865d0.chunk.css"
+  },
+  {
+    "revision": "870e99af248143a0b36f",
+    "url": "/Success/static/js/2.add538f7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Success/static/js/2.add538f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c6b3de7dde2b6a56b0d",
+    "url": "/Success/static/js/main.bcf21de0.chunk.js"
+  },
+  {
+    "revision": "96361480662702bfc9cb",
+    "url": "/Success/static/js/runtime-main.5cd4639c.js"
+  }
+]);
